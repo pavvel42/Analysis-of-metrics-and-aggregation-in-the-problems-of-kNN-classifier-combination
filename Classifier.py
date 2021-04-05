32782ebf1de440ed1a8e8edff74c6c1cbbe997c2
pavvel42 <pavvel.durda@gmail.com>
2021-04-05 14:04:07 +0200

    def predict(self, X, y, t, aggregation='arithmetic'):         prob = self.kNN.predict_proba(X)

--- Classifier.py
+++ Classifier.py
@@ -1,3 +1,4 @@
+from sklearn.model_selection import StratifiedKFold
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
@@ -6,8 +7,13 @@ from sklearn.metrics import recall_score
 from sklearn.metrics import f1_score
 from sklearn.metrics import roc_auc_score
 from sklearn.metrics import roc_curve
+from sklearn.svm import SVR
+from sklearn.feature_selection import RFECV
 # https://scikit-learn.org/stable/modules/classes.html#module-sklearn.metrics
 import numpy as np
+import random
+import pandas as pd
+from statistics import stdev
 from agregacje import amean  # Arithmetic
 from agregacje import qmean  # Quadratic
 from scipy.stats import hmean  # Harmonic
@@ -21,6 +27,8 @@ class Classifier:
         self.p = p
         self.kNN = KNeighborsClassifier(n_neighbors=self.n_neighbors, metric=self.metric, p=self.p)
         self.pi = []
+        self.aucs = []
+        self.accs = []
 
     def classifier(self):
         return self.kNN
@@ -28,55 +36,51 @@ class Classifier:
     def info(self):
         print(f"kNN metric {self.metric} n_neighbors {self.n_neighbors} p {self.p}")
 
-    def fit(self, selector, y):
+    def fit(self, X, y):
+        random_col = random.randint(1,
+                                    y.size)  # dla testów lokalnych aby przyśpieszyć uzyskanie wyniku na sztywno ustawiam tą wartość min
+        # print('random_col ', random_col, ' s ', i + 1)
+        estimator = SVR(kernel="linear")
+        selector = RFECV(estimator, min_features_to_select=random_col, step=1, cv=5, n_jobs=-1).fit_transform(X, y)
+        # print(selector)
+        # print(y)
         self.kNN.fit(selector, y)
-        self.pi.append(self.kNN.predict_proba(selector)[:, 0])
+        #self.pi.append(self.kNN.predict_proba(selector)[:, 0])
+        # self.aucs.append(self.auc(self.pi, y))
+        # self.accs.append(self.accuracy(self.pi, y))
 
     def return_pi(self):
         return self.pi
 
-    def aggregation_arithmetic(self, t, y):
+    def predict(self, X, y, t, aggregation='arithmetic'):
+        prob = self.kNN.predict_proba(X)
+        self.pi.append(prob[:, 1])
         pi = np.array(self.return_pi())
-        agregacja_pi = amean(pi)
-        # print("Agregacja arytmetyczna ", agregacja_pi)
-        pi = np.where(agregacja_pi > t, 0, 1)
-        self.classifier_score(self.aggregation_arithmetic.__name__, pi, y)
-        return pi
-
-    def aggregation_quadratic(self, t, y):
-        pi = np.array(self.return_pi())
-        agregacja_pi = qmean(pi)
-        # print("Agregacja kwadratowa ", agregacja_pi)
-        pi = np.where(agregacja_pi > t, 0, 1)
-        self.classifier_score(self.aggregation_quadratic.__name__, pi, y)
-        return pi
-
-    def aggregation_harmonic(self, t, y):
-        pi = np.array(self.return_pi())
-        agregacja_pi = hmean(pi)
-        # print("Agregacja harmoniczna ", agregacja_pi)
-        pi = np.where(agregacja_pi > t, 0, 1)
-        self.classifier_score(self.aggregation_harmonic.__name__, pi, y)
-        return pi
-
-    def aggregation_geometric(self, t, y):
-        pi = np.array(self.return_pi())
-        agregacja_pi = gmean(pi)
-        # print("Agregacja geometryczna ", agregacja_pi)
-        pi = np.where(agregacja_pi > t, 0, 1)
-        self.classifier_score(self.aggregation_geometric.__name__, pi, y)
-        return pi
+        if aggregation == 'arithmetic':
+            pimean = amean(pi)
+        elif aggregation == 'quadratic':
+            pimean = qmean(pi)
+        elif aggregation == 'harmonic':
+            pimean = hmean(pi)
+        elif aggregation == 'geometric':
+            pimean = gmean(pi)
+        pred = np.where(pimean > t, 0, 1)
+        self.aucs.append(self.auc(pi=pimean, y=y))
+        self.accs.append(self.accuracy(pi=pred, y=y))
+        self.classifier_score(aggregation)
 
     def classifier_score(self, method_name, pi, y):
         score = {}
         score[self.metric] = method_name
-        score[confusion_matrix.__name__] = self.conf_matrix(pi, y)
-        score[self.accuracy.__name__] = self.accuracy(pi, y)
-        score[self.precision.__name__] = self.precision(pi, y)
-        score[self.recall.__name__] = self.recall(pi, y)
-        score[self.f1.__name__] = self.f1(pi, y)
-        score[self.auc.__name__] = self.auc(pi, y)
-        score[self.roc.__name__] = self.roc(pi, y)
+        # score[confusion_matrix.__name__] = self.conf_matrix(pi, y)
+        # score[self.accuracy.__name__] = self.accuracy(pi, y)
+        # score[self.precision.__name__] = self.precision(pi, y)
+        # score[self.recall.__name__] = self.recall(pi, y)
+        # score[self.f1.__name__] = self.f1(pi, y)
+        # score[self.auc.__name__] = self.auc(pi, y)
+        # score[self.roc.__name__] = self.roc(pi, y)
+        score[self.aucs.__name__] = np.mean(self.aucs)
+        score[self.accs.__name__] = stdev(self.aucs)
         print(score)
 
     def conf_matrix(self, pi, y):
--- algorytm.py
+++ algorytm.py
@@ -1,23 +1,29 @@
-import random
-from sklearn.svm import SVR
-from sklearn.feature_selection import RFECV
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder
+from sklearn.model_selection import StratifiedKFold
 import pandas as pd
 import numpy as np
 from Classifier import Classifier
+from timeit import default_timer as timer
+
 # pip freeze > requirements.txt
 
-metricList = ['euclidean', 'manhattan', 'chebyshev', 'hamming', 'canberra', 'braycurtis']
+metricList = ['euclidean', 'manhattan', 'chebyshev', 'hamming', 'canberra', 'braycurtis']  # +minkowski wersje
 # https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html
 
 T = pd.read_csv("data/colonTumor.csv")
 print(T.shape)
+scaler = MinMaxScaler()
 X = T.iloc[:, 1:T.shape[1]]
+X = scaler.fit_transform(X)
 y = T.iloc[:, 0]
 y = np.where(y == 'negative', 0, 1)
+y = LabelEncoder().fit_transform(y)
+
 kNNDict = {}
 
 
 def algorytm(X, y, k=3, s=5, t=0.5, p=[2]):
+    start = timer()
     for metric in metricList:
         kNN = Classifier(n_neighbors=k, metric=metric, p=None)
         kNNDict[metric] = kNN
@@ -29,21 +35,23 @@ def algorytm(X, y, k=3, s=5, t=0.5, p=[2]):
     # print(kNNDict)
 
     for i in range(s):
-        random_col = random.randint(45, T.shape[0])  # dla testów lokalnych aby przyśpieszyć uzyskanie wyniku na sztywno ustawiam tą wartość min
-        print('random_col ', random_col, ' s ', i + 1)
-        estimator = SVR(kernel="linear")
-        selector = RFECV(estimator, min_features_to_select=random_col, step=1, cv=5, n_jobs=-1).fit_transform(X, y)
-        print(selector)
-        print(y)
-        for metric in metricList:
-            kNNDict[metric].fit(selector=selector, y=y)
+        skf = StratifiedKFold(n_splits=10)
+        skf.get_n_splits(X, y)
+        for train_index, test_index in skf.split(X, y):
+            X_train, X_test = X[train_index], X[test_index]
+            y_train, y_test = y[train_index], y[test_index]
+            for metric in metricList:
+                kNNDict[metric].fit(X=X_train, y=y_train)
+                kNNDict[metric].predict(aggregation='arithmetic', X=X_test, y=y_test, t=t)
 
-    for metric in metricList:
-        kNNDict[metric].info()
-        kNNDict[metric].aggregation_arithmetic(t=t, y=y)
-        kNNDict[metric].aggregation_quadratic(t=t, y=y)
-        kNNDict[metric].aggregation_harmonic(t=t, y=y)
-        kNNDict[metric].aggregation_geometric(t=t, y=y)
+    # for metric in metricList:
+    # kNNDict[metric].info()
+    # kNNDict[metric].aggregation_arithmetic(t=t, y=y)
+    # kNNDict[metric].aggregation_quadratic(t=t, y=y)
+    # kNNDict[metric].aggregation_harmonic(t=t, y=y)
+    # kNNDict[metric].aggregation_geometric(t=t, y=y)
+    end = timer()
+    print('Czas Wykonania: ', end - start)
 
 
-algorytm(X=X, y=y, p=[1, 2, 3, 4])
+algorytm(X=X, y=y, p=[1, 2, 3, 4], s=1)
